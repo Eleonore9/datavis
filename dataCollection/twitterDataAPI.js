7e56b1f73e48e8616a47df7f27a51c44d619b8e8
Eleonore9 <eleonore.mayola@ymail.com>
2015-05-23 12:55:19 +0100

    Using Twitter api, playing w/D3

--- dataCollection/twitterDataAPI.js
+++ dataCollection/twitterDataAPI.js
@@ -1,17 +1,19 @@
-var Twit = require('twit');
 var fs = require('fs');
-var twit = new Twit({
-	consumer_key : '???',
-	consumer_secret : '???',
-	access_token : '???',
-	access_token_secret : '???'
-});
+
+var config = require('./config.js');
 
 var uk = [ '-9.23', '49.84', '2.69', '60.85' ];
-var stream = twit.stream('statuses/filter', { locations: uk })
+var stream = config.stream('statuses/filter', { locations: uk })
 
 stream.on('tweet', processTweet);
 
+var log = fs.createWriteStream('tweets.log');
+
 function processTweet(tweet) {
-  console.log(tweet);
+    var regexp = /[Cc]oding/g;
+    if (regexp.test(tweet.text)){
+        console.log(tweet.text);
+        var strTweet = JSON.stringify(tweet);
+        log.write(strTweet);
+    }
 };
